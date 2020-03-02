2aa0e8a198444fe076c9c83c76b12f1e64fadd77
moorcan <moorcan85@gmail.com>
2020-03-01 17:47:26 -0800

    2020/03/02 added to project

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Card
+{
+    public string Name { get; set; }
+
+    public Card()
+    {
+        Name = "";
+    }
+
+    public bool IsFaceUp { get; private set; }
+    
+
+public void flip()
+    {
+        IsFaceUp = !IsFaceUp; 
+    }
+
+}
+
---
+++ Assets/Scripts/PokerDeck.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokerDeck : Deck
+{
+    public PokerDeck()
+    {
+
+
+
+        for (int i = 1; i < 14; i++)
+        {
+            List<string> suits = new List<string>();
+            suits.Add("Spades");
+            suits.Add("Diamond");
+            suits.Add("Club");
+            suits.Add("Heart");
+
+            foreach (string suit in suits)
+            {
+                PokerCard card = new PokerCard();
+                card.Suit = suit;
+                card.Value = i;
+                AddToTop(card);
+            }
+
+
+        }
+
+
+    }
+}
